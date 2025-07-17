2bc26639b29c53ddee2fdffaba62ae8cdedd031c
babulsraj <babul.raj@impelsys.com>
2025-07-17 13:27:22 +0530

    fix: improve image downloading implementation

--- InfiniteScroll_Operations/ContentView.swift
+++ InfiniteScroll_Operations/ContentView.swift
@@ -1,9 +1,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var presenter = Presenter()
+    
     var body: some View {
-        VStack {
-            Text("Hello, Infinite Scroll!")
+        List(presenter.users, id: \.id) { user in
+            HStack {
+                if let image = user.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .clipShape(Circle())
+                } else {
+                    Circle()
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(width: 50, height: 50)
+                }
+                
+                Text(user.login)
+                    .font(.headline)
+            }
+            .onAppear {
+                if presenter.shouldLoadData(id: user.id) {
+                    presenter.getData(index: user.id)
+                }
+                presenter.getImage(index: presenter.users.firstIndex(of: user) ?? 0)
+            }
+        }
+        .onAppear {
+            if presenter.users.isEmpty {
+                presenter.getData(index: 0)
+            }
         }
     }
 }
--- InfiniteScroll_Operations/Presenter.swift
+++ InfiniteScroll_Operations/Presenter.swift
@@ -1,5 +1,181 @@
 import Foundation
+import UIKit
+
+struct User: Codable, Equatable {
+    var id: Int
+    var avatar_url: String
+    var login: String
+    var image: UIImage? = nil
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, avatar_url, login
+    }
+    
+    init(id: Int, avatar_url: String, login: String) {
+        self.id = id
+        self.avatar_url = avatar_url
+        self.login = login
+        self.image = nil
+    }
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+    }
+}
 
 class Presenter: ObservableObject {
-    // Add your presenter logic here
+    @Published var users: [User] = []
+    private var operations: [Int: ImageDownload] = [:]
+    let downloadQueue = OperationQueue()
+    
+    init() {
+        downloadQueue.maxConcurrentOperationCount = 5
+    }
+    
+    func getData(index: Int) {
+        let url = URL(string: "https://api.github.com/users?per_page=30&since=\(index+1)")!
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { [weak self] data, response, error in
+            if let error = error {
+                print("Error fetching users: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            do {
+                let users = try JSONDecoder().decode([User].self, from: data)
+                DispatchQueue.main.async {
+                    self?.users.append(contentsOf: users)
+                }
+            } catch {
+                print("Error parsing users: \(error)")
+            }
+        }.resume()
+    }
+    
+    func getImage(index: Int) -> Bool {
+        guard index < users.count else { return false }
+        
+        // Cancel existing operation if any
+        if let existingOperation = operations[index] {
+            existingOperation.cancel()
+            operations.removeValue(forKey: index)
+        }
+        
+        let operation = ImageDownload(user: users[index])
+        operations[index] = operation
+        
+        operation.completionBlock = { [weak self, weak operation] in
+            guard let operation = operation, !operation.isCancelled else { return }
+            
+            DispatchQueue.main.async {
+                if let image = operation.downloadedImage {
+                    self?.users[index].image = image
+                }
+                self?.operations.removeValue(forKey: index)
+            }
+        }
+        
+        downloadQueue.addOperation(operation)
+        return true
+    }
+    
+    func shouldLoadData(id: Int) -> Bool {
+        return (users.count - 3) == id
+    }
+}
+
+class ImageDownload: AsyncOperation {
+    private let user: User?
+    private let url: URL?
+    private var urlSession: URLSession?
+    private var dataTask: URLSessionDataTask?
+    private(set) var downloadedImage: UIImage?
+    
+    init(user: User?) {
+        self.user = user
+        self.url = URL(string: user?.avatar_url ?? "")
+        super.init()
+    }
+    
+    override func main() {
+        guard !isCancelled else {
+            state = .isFinished
+            return
+        }
+        
+        guard let url = url else {
+            state = .isFinished
+            return
+        }
+        
+        urlSession = URLSession(configuration: .default)
+        dataTask = urlSession?.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self, !self.isCancelled else { return }
+            
+            if let error = error {
+                print("Error downloading image: \(error)")
+                self.state = .isFinished
+                return
+            }
+            
+            if let data = data, let image = UIImage(data: data) {
+                self.downloadedImage = image
+            }
+            
+            self.state = .isFinished
+        }
+        
+        dataTask?.resume()
+    }
+    
+    override func cancel() {
+        dataTask?.cancel()
+        super.cancel()
+    }
+}
+
+class AsyncOperation: Operation {
+    enum State: String {
+        case isReady
+        case isExecuting
+        case isFinished
+    }
+    
+    var state: State = .isReady {
+        willSet {
+            willChangeValue(forKey: state.rawValue)
+            willChangeValue(forKey: newValue.rawValue)
+        }
+        didSet {
+            didChangeValue(forKey: oldValue.rawValue)
+            didChangeValue(forKey: state.rawValue)
+        }
+    }
+    
+    override var isAsynchronous: Bool { true }
+    override var isExecuting: Bool { state == .isExecuting }
+    override var isFinished: Bool {
+        if isCancelled && state != .isExecuting { return true }
+        return state == .isFinished
+    }
+    
+    override func start() {
+        guard !isCancelled else {
+            state = .isFinished
+            return
+        }
+        state = .isExecuting
+        main()
+    }
+    
+    override func cancel() {
+        state = .isFinished
+        super.cancel()
+    }
 }
